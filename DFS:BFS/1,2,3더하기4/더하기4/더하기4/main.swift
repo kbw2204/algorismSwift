2edb7b0d670380870526ba184d72220849be43c1
강병우 <kbw2204@icloud.com>
2020-05-19 23:31:20 +0900

    1,2,3더하기4 문제

---
+++ DFS:BFS/1,2,3더하기4/더하기4/더하기4/main.swift
@@ -0,0 +1,34 @@
+//
+//  main.swift
+//  더하기4
+//
+//  Created by 강병우 on 2020/05/19.
+//  Copyright © 2020 강병우. All rights reserved.
+//
+
+import Foundation
+
+func dfs(result: inout Set<[Int]>, sum: Int, target: Int, list: [Int]) {
+    if sum == target {
+        result.insert(list.sorted())
+        return
+    }
+    
+    for i in 1 ... 3 {
+        if sum + i > target {
+            break
+        }
+        var vList = list
+        vList.append(i)
+        dfs(result: &result, sum: sum + i, target: target, list: vList)
+    }
+}
+let count: Int = Int(readLine()!)!
+for _ in 0 ..< count {
+    let input: Int = Int(readLine()!)!
+    var result: Set<[Int]> = []
+    dfs(result: &result, sum: 0, target: input, list: [])
+    print(result.count)
+}
+
+
