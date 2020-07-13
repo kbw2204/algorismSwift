4db1cb235e365f1e752e15c9ee0f3b752dd63d2c
강병우 <kbw2204@icloud.com>
2020-07-13 14:42:14 +0900

    아스키코드 풀이

---
+++ 문자열/아스키코드/아스키코드/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  아스키코드
+//
+//  Created by 강병우 on 2020/07/13.
+//  Copyright © 2020 강병우. All rights reserved.
+//
+
+let ipt = readLine()!
+print(UnicodeScalar(ipt)!.value)
