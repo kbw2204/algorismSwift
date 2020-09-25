b6e48ee7753e64966be6c2ce895d3dcc81f6db37
강병우 <kbw2204@icloud.com>
2020-09-25 21:42:31 +0900

    다리를 지나는 트럭 문제풀이

--- stack, queue/다리를 지나는 트럭/다리를 지나는 트럭/main.swift
+++ stack, queue/다리를 지나는 트럭/다리를 지나는 트럭/main.swift
@@ -4,48 +4,44 @@
 //
 //  Created by 강병우 on 2020/04/22.
 //  Copyright © 2020 강병우. All rights reserved.
-// 문제 이해안됨 =_= 문제가 잘못된 것 같음
-
-import Foundation
 
 func solution(_ bridge_length:Int, _ weight:Int, _ truck_weights:[Int]) -> Int {
-    var result = 0
-    var sum = 0
-    var count = 0
-    var queue = truck_weights
-    while !queue.isEmpty {
-        while weight > sum {
-            guard let temp = queue.first else {
-                result += count*(bridge_length-1)
-                break
-            }
-            if sum + temp <= weight {
-                sum += queue.removeFirst()
-                result += 1
-                count += 1
-            } else {
-                result += count*(bridge_length-1)
-                count = 0
-                sum = 0
-                break
+    var time = 0
+    var truckList = truck_weights.reversed().map{Int($0)}
+    var onTructList: [Int] = []
+    var 진행거리: [Int] = []
+    
+    let run: (Int) -> () = { i in
+        onTructList.append(i)
+        진행거리.append(1)
+    }
+    
+    let checkArrive = {
+        if let first = 진행거리.first {
+            if first > bridge_length {
+                onTructList.removeFirst()
+                진행거리.removeFirst()
             }
         }
     }
     
-    return result
-}
-
-let testInput = [2,100,100]
-let testInput2 = [10,100,100]
-let testInput3 = [[7,4,5,6],[10],[10,10,10,10,10,10,10,10,10,10]]
-let testOutput = [8,101,110]
-
-for i in 0 ..< testInput.count {
-    let output = solution(testInput[i], testInput2[i],testInput3[i])
-    if output == testOutput[i] {
-        print("성공")
-    } else {
-        print("실패: \(testOutput[i]) : \(output)")
+    let going = {
+        진행거리 = 진행거리.map{$0+1}
+        time += 1
     }
+    
+    while !onTructList.isEmpty || !truckList.isEmpty {
+        going()
+        checkArrive()
+        if let first = truckList.last {
+            let sum = onTructList.reduce(0, +)
+            if sum + first <= weight {
+                run(truckList.removeLast())
+            }
+        }
+    }
+    
+    return time
 }
 
+print(solution(2, 10, [7,4,5,6]))
