865d0f551663ab26d338a379dfafad54dbacba50
강병우 <kbw2204@icloud.com>
2020-11-10 23:04:32 +0900

    풍선 터트리기 시간 줄이기

--- 문제이해/풍선 터트리기/풍선 터트리기/main.swift
+++ 문제이해/풍선 터트리기/풍선 터트리기/main.swift
@@ -6,14 +6,29 @@
 //
 
 func solution(_ a:[Int]) -> Int {
+    var rst = 2
+    var rm = a[a.index(a.startIndex, offsetBy: 2)...].min()!
+    var lm = a[0]
     let check: (Int) -> Bool = { i in
-        let lm = a[..<a.index(a.startIndex, offsetBy: i)].min()!
-        if a[i] < lm { return true }
-        let rm = a[a.index(a.startIndex, offsetBy: i+1)...].min()!
-        return a[i] < rm
+        if a[i] == rm { rm = a[a.index(a.startIndex, offsetBy: i+1)...].min()! }
+        
+        if a[i] < lm {
+            lm = [lm, a[i]].min()!
+            return true
+        }
+        if a[i] < rm {
+            return true
+        }
+        return false
     }
     
-    return Array(1...a.count-2).map{check($0)}.filter{$0==true}.count + 2
+    for i in 1 ..< a.count-1 {
+        if check(i) {
+            rst += 1
+        }
+    }
+
+    return rst
 }
 
 let a = [-16,27,65,-2,58,-92,-71,-68,-61,-33]
